0ae4863a80157e954fa3198947cd5a74a760390d
java1 <monkey@gmail.com>
2018-07-10 21:47:53 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+//        NetworkManager.disabless();
+        //
+        ApiContextInitializer.init();
+
+        TelegramBotsApi api = new TelegramBotsApi();
+
+        try {
+            api.registerBot(new CivilEngineeringBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
